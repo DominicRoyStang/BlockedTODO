980522dfd33d4b03e808490040910b79024969db
Dominic Roy-Stang <dominicroystang@gmail.com>
2026-08-03 03:13:24 -0700

    Delete files that won't be needed as we switch to running in actions

--- services/backend/src/github/utils/index.js
+++ services/backend/src/github/utils/index.js
@@ -1,5 +1,2 @@
-export {createAppClient, createInstallationClient} from './githubClients.js';
-export {default as downloadRepository} from './downloadRepository.js';
 export {default as getIssue} from './getIssue.js';
 export {default as createIssue} from './createIssue.js';
-export {default as getInstallationRepositories} from './getInstallationRepositories.js';
--- services/backend/src/utils/base64.js
+++
@@ -1,3 +0,0 @@
-export const base64Encode = (text) => Buffer.from(text).toString('base64');
-
-export const base64Decode = (text) => Buffer.from(text, 'base64').toString();
